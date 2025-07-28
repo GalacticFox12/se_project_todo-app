47f1922b676ddd45429cfcfe8f16b6f7624d0045
Kevin Edwards <herbie92@gmail.com>
2025-07-27 21:20:18 -0500

    refactor: add todo items to DOM in section.js

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,13 @@ import { v4 as uuidv4 } from "https://jspm.dev/uuid";
 import { initialTodos, validationConfig } from "../utils/constants.js";
 import Todo from "../components/Todo.js";
 import FormValidator from "../components/FormValidator.js";
+import Section from "../components/Section.js";
 
 const addTodoButton = document.querySelector(".button_action_add");
 const addTodoPopup = document.querySelector("#add-todo-popup");
 const addTodoForm = addTodoPopup.querySelector(".popup__form");
 const addTodoCloseBtn = addTodoPopup.querySelector(".popup__close");
-const todosList = document.querySelector(".todos__list");
+const addItem = document.querySelector(".todos__list");
 
 const openModal = (modal) => {
   modal.classList.add("popup_visible");
@@ -33,8 +34,18 @@ addTodoCloseBtn.addEventListener("click", () => {
 
 const renderTodo = (item) => {
   const todo = generateTodo(item);
-  todosList.append(todo);
+  addItem.append(todo);
 };
+
+const section = new Section({
+  items: initialTodos,
+  renderer: (item) => {
+    renderTodo(item);
+  },
+  containerSelector: ".todos__list",
+});
+
+section.renderItems();
 
 addTodoForm.addEventListener("submit", (evt) => {
   evt.preventDefault();
@@ -49,10 +60,6 @@ addTodoForm.addEventListener("submit", (evt) => {
   renderTodo(values);
   closeModal(addTodoPopup);
   newTodoValidator.resetValidation();
-});
-
-initialTodos.forEach((item) => {
-  renderTodo(item);
 });
 
 const newTodoValidator = new FormValidator(validationConfig, addTodoForm);
